3be6ed33540f296449bf8161082bd6c1ff504214
dorLottan <dor.lottan@gmail.com>
2020-10-01 19:14:55 +0300

    add homepage field to package.json

---
+++ docs/precache-manifest.e14a423973dd61367dd3eb33c91d9fad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac773017586579f92d8f0e4b4446571",
+    "url": "/React-Movies-DB/index.html"
+  },
+  {
+    "revision": "0533e14e70f59eaf17fe",
+    "url": "/React-Movies-DB/static/js/2.16e08483.chunk.js"
+  },
+  {
+    "revision": "f0889c706f83a651cd6b",
+    "url": "/React-Movies-DB/static/js/main.ed95f54f.chunk.js"
+  },
+  {
+    "revision": "65700253b2e24c1f2fb4",
+    "url": "/React-Movies-DB/static/js/runtime~main.6bdf5288.js"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/React-Movies-DB/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/React-Movies-DB/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e2470ff57e3726f91833591a451ffe8.js"
+  "/React-Movies-DB/precache-manifest.e14a423973dd61367dd3eb33c91d9fad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Movies-DB/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
